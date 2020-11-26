530f65ffef2fb54f7e35cd51acc9a900910a74f9
LJLee37 <ljlee3759@gmail.com>
2020-11-26 20:45:09 +0900

    sample

--- HelloWorld/LJLeeHelloWorld.cpp
+++ HelloWorld/LJLeeHelloWorld.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
-using namespace std;
-int main()
+/* file name: LJLeeHelloWorld.cpp
+ * Created: can't remember
+ * modified: Thu, Nov 26th, 20:35
+ * author: LJLee37
+ */
+#include <iostream> //iostream파일을 추가한다.
+using namespace std; //std namespace를 사용한다.
+int main() //int: integer, main: start here!
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    cout << "Hello World!" << endl;//print hello world and end line
+    return 0; //return 0 to caller, 0: normal.
 }
