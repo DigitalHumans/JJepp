cb221323c3de402d8c5e49a4dd508fb6555b6192
NerDiDream <nerdydream@naver.com>
2020-11-26 20:47:39 +0900

    2020.11.26 수업

---
+++ 20201126/NerDiDreamHelloWorld.cpp
@@ -0,0 +1,13 @@
+/* file name: NerDiDreamHelloWorld.cpp
+** Created: can't remember
+** modified: Thu, Nov 26th, 20:35
+** author: ner-di-dream
+*/
+
+#include <iostream> //iostream 파일을 추가한다.
+using namespace std; //std namespace를 사용한다.
+int main() //int: integer, main: start here!
+{
+	cout << "Hello World" << endl; //print Hello World! and end line
+	return 0; //return 0 to caller, 0: normal
+}
