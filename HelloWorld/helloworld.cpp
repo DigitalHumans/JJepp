6cdb6ba5e4d7fb3d4f7243870362e82e78ca90b2
skyminju9 <skyminju9@naver.com>
2020-11-26 20:45:33 +0900

    주석

--- HelloWorld/helloworld.cpp
+++ HelloWorld/helloworld.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
-using namespace std;
+/* file name : helloworld.cpp
+ * created : can't remember
+ * modified : Thu, Nov 26th, 20:35
+ * author : kimminju
+ */
+#include <iostream> //iostream 파일을 추가한다.
+using namespace std; //std라는 이름의 namespace를 사용한다. (namespace:여러가지 데이터 타입과 각종 함수의 집합)
 
-int main(){
-    cout<<"Hello world"<<endl;
-    return 0;
+int main(){ //int:integer, main:start here
+    cout<<"Hello world"<<endl; //print hello world and end line(줄바꿈)
+    return 0; //return 0 to caller, 0 : 이상 없음
 }
