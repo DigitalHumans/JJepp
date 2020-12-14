1bbd3cfec0e9c011f52651d078029c52025693c9
spenshark <xzx01299@naver.com>
2020-12-14 20:08:53 +0900

    주석주석

--- HelloWorld/shark_helloworld.cpp
+++ HelloWorld/shark_helloworld.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "Hello world" << endl;
-    return 0;
+#include <iostream> // 입출력을 위한 헤더파일
+using namespace std; // 표준의 namespace 사용
+int main()
+{
+    cout << "Hello world" << endl; // Hello world를 출력
+    return 0; // 0을 리턴
 }
