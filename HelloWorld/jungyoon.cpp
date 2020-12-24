cbba4b22ca172d3d35fb9e001a35600576fd78ed
ekg1229 <ekg1229@naver.com>
2020-12-25 01:00:57 +0900

    20201221 과제 피드백 반영

--- HelloWorld/jungyoon.cpp
+++ HelloWorld/jungyoon.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-using namespace std;
+#include <iostream>		// 헤더파일의 선언문
+using namespace std;	// 표준 네임스페이스 사용
 
 int main(){
-	cout<<"Hello World"<<endl;	//hello world 출력
-	return 0;	//int main이므로 return 0;을 해준다.
+	cout<<"Hello World"<<endl;	// "Hello World"를 표준 출력 객체로 전달->endl객체를 표준 출력 객체에 전달
+	return 0;	// int main이므로 return 0;을 해준다.
 }
