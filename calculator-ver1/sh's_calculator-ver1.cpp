1520c1f67a65a772d56317c06058eb985767ea75
KSoonhyung <ccsa3321@naver.com>
2020-12-28 17:59:33 +0900

    Git test

---
+++ calculator-ver1/sh's_calculator-ver1.cpp
@@ -0,0 +1,15 @@
+/*file name: sh's_calculator-ver1.cpp
+created: 2020-12-28
+modified: 2020-12-28
+author: Kwon  Soonhyung
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World" << endl;
+
+}
