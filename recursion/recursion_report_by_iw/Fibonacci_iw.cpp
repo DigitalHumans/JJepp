a47ac2a89a49e30b2fc56c8ce41a97f250f2b7ef
kimz1121 <kimz1121@naver.com>
2021-01-03 20:19:57 +0900

    report_1

--- recursion/recursion_report_by_iw/Fibonacci_iw.cpp
+++ recursion/recursion_report_by_iw/Fibonacci_iw.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 
+using namespace std;
 
-long Fibonacci_recursion(long N_0, long N_1, int )
+int* Nuber_of_N = 0;
+
+long Fibonacci_recursion(long N_0, long N_1, int* N)
 {
-
+    if(*N != 0)
+    {
+        *N - 1;
+        cout <<Fibonacci_recursion(N_1, N_0+N_1 ,N)<<", ";
+    }
+    cout<<N_0 + N_1<<endl;
+    return N_0 + N_1;
 }
 
 
 int main()
 {
-
-
+    *Nuber_of_N = 5;
+    Fibonacci_recursion(1, 1, Nuber_of_N);
 }
