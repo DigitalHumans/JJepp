0462262e658d33d2ac1db3c3eac3dcb72078b71f
jinnyfruit <jinnyfruit0527@gmail.com>
2021-01-04 12:26:23 +0900

    homework

---
+++ recursion/recursion_JiWoo/fac.cpp
@@ -0,0 +1,28 @@
+/*factorial in CPP
+ detail: using recursion fuction
+
+ made by: Ji Woo, Kim
+ modified: 01.03, 2021
+ */
+#include <iostream>
+using namespace std;
+int factorial(int n) {
+
+	if (n < 2) {	//turmination condition
+		return n;	//recursive ends.
+	}
+	else
+		return n * factorial(n - 1);	//else,recursive steps
+}
+
+int main() {
+	int num;
+	int value;
+
+	cout<<"type a integer factorial(ex-3!)"<<endl;
+	cin>>num;	//get a integer
+
+	value = factorial(num);	//put a factorial value into "value"
+	cout<<"\n"<<num<<"! is "<<value<<endl;	//show the value
+	return 0;
+}
