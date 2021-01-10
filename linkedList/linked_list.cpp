81f3b11f924fada8cad16c784b0c0750c28d40bd
Qstion <qstion05@gmail.com>
2021-01-11 02:35:29 +0900

    선생님 정말 메소드는 진짜 모르겠어요 죄송합니다 ㅠㅠㅠㅠㅠ

--- linkedList/linked_list.cpp
+++ linkedList/linked_list.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
+#include<malloc.h>
 using namespace std;
 
 struct List_node
-{
+{	
+
 	int DATA = 0;
 	struct List_node *NEXT_NODE;
+	List_node(int D):DATA(D){NEXT_NODE = NULL;}
 };
 
 int main(){
@@ -17,11 +20,7 @@ int main(){
 	for(int i = 0; i < 5; i++){
 		cout << "자연수를 입력해주세요 : ";
 		cin >> input_data;
-
-		NEW_NODE = new List_node(); //새로운 노드를 선언하고.
-		NEW_NODE -> DATA = input_data; // 새로운 노드에 입력받은 자연수 정보를 저장하고.
-		NEW_NODE -> NEXT_NODE = NULL; // 새로운 노드의 NEXT변수를 초기화한다.
-
+		NEW_NODE = new List_node(input_data); //새로운 노드를 선언하고.
 		if(HEAD == NULL) //만약, HEAD가 가리키는 노드가 없으면,
 			HEAD = NEW_NODE; //HEAD는 추가된 노드를 가르킨다.
 		else//HEAD가 가리키는 노드가 있으면,
@@ -35,5 +34,20 @@ int main(){
 	for(int j = 0; j < 4; j++){
 		CURSOR = CURSOR -> NEXT_NODE; //CURSOR이 현재 가리키는 노드의 다음 노드를 가리킨다.
 		cout<<j+2<<"번째 노드에 저장된 데이터: "<<CURSOR->DATA<<""<<endl; //CURSOR이 가리키는 노드에 저장된 데이터값을 출력한다.
+	}
+
+	//할당 해제
+	if (HEAD == NULL)
+		return 0;
+	else{
+		List_node *delNode = HEAD; //delNode를 만들고, HEAD를 가르킴.
+		List_node *delNextNode = HEAD -> NEXT_NODE;//delNextNode를 만들고, HEAD의 NEXT 변수를 저장함.
+		delete(delNode); //delNode에 배정된 Heap 메모리를 할당 해제 함.
+		for(int k = 0; k < 4; k++)
+		{
+			delNode = delNextNode; //delNode는 delNextNode가 가르키는 NEXT 변수가 가르키는 노드를 가르킴.
+			delNextNode = delNextNode->NEXT_NODE; //delNextNode는 현재 노드의 NEXT변수를 저장함.
+			delete(delNode); ////delNode에 배정된 Heap 메모리를 할당 해제 함.
+		}
 	}
 }
