3bdd46110ec4b2b364f95d6a3c871cf40a0058ad
bbasung1 <bbasung1@daum.net>
2021-01-18 15:01:45 +0900

    add filesystem

--- gibumfile/gibumfile.cpp
+++ gibumfile/gibumfile.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
 using namespace std;
+namespace fs=std::filesystem;
 int main(){
     char a[5000];
     char b[5000];
-    cout<<"경로를 입력 하시오:";
+    cout<<"바꾸려는 파일의 이름들은?";
     cin>>a;
-    cout<<"바꿀 이름을 입력하시오";
+    cout<<"바꿀 이름은?";
     cin>>b;
-    rename(a,b);
+    for(auto& p: fs:: recursive_directory_iterator(path)){
+
+    }
 }
