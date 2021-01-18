5e856ef5a7b6093f36fbcc55bb876117b63be50c
bbasung1 <bbasung1@daum.net>
2021-01-18 16:22:55 +0900

    first version

--- gibumfile/gibumfile.cpp
+++ gibumfile/gibumfile.cpp
@@ -6,7 +6,7 @@ using namespace std;
 namespace fs=std::filesystem;
 int main(){
     char a[5000];
-    char b[5000];
+    string b;
     cout<<"바꾸려는 파일의 이름들은?";
     cin>>a;
     cout<<"바꿀 이름은?";
@@ -18,8 +18,9 @@ int main(){
         if(tp1.find(a)!=-1){          
             int poin=tp1.find(".",1);
             tp1.erase(0,poin);
-            char new[1000]=strcat(b+tp1.c_str());
-            rename(t2.c_str(),);
+            string c=b;
+            c+=tp1;
+            rename(t2.c_str(),c.c_str());
         }
     }
 }
