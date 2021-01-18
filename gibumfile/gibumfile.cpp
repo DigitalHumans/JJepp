fdf2996f691ea7978655e83e4e757e6c8ae3052d
bbasung1 <bbasung1@daum.net>
2021-01-18 16:05:13 +0900

    failed1-cstr not working

--- gibumfile/gibumfile.cpp
+++ gibumfile/gibumfile.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include <cstdio>
 using namespace std;
 namespace fs=std::filesystem;
 int main(){
@@ -10,7 +11,15 @@ int main(){
     cin>>a;
     cout<<"바꿀 이름은?";
     cin>>b;
+    string path="./";
     for(auto& p: fs:: recursive_directory_iterator(path)){
-
+        string tp1=p.path().filename();
+        string t2=p.path().filename();
+        if(tp1.find(a)!=-1){          
+            int poin=tp1.find(".",1);
+            tp1.erase(0,poin);
+            char new[1000]=strcat(b+tp1.c_str());
+            rename(t2.c_str(),);
+        }
     }
 }
