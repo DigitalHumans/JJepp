68c5572db93f7e7dfecb59bc3efbe4e43ca74dc4
bbasung1 <bbasung1@daum.net>
2021-01-20 18:04:33 +0900

    change var

--- gibumfile/gibumfile.cpp
+++ gibumfile/gibumfile.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 namespace fs=std::filesystem;
 int main(){
-    int a;
+    int choose;
     cout<<"다음중 선택세요(1.파일명 일괄 변경/2.특정 문자 지우기):";
-    cin>>a;
-    if( a==1){
+    cin>>choose;
+    if( choose==1){
     string target;
     string change;
     cout<<"바꾸려는 파일들에 공통적으로 들어 있는것은?";
@@ -33,23 +33,26 @@ int main(){
         }
     }
     }
-    else if(a==2){
-        int b;
+    else if(choose==2){
+        int delposit;
         cout<<"앞에서부터 몇자리까지 지우실지를 입력해주세요:";
-        cin>>b;
+        cin>>delposit;
         cout<<"모든파일에 적용하시겠습니까?특정 문자열이 들어가 있을 경우 적용하시겠습니까?(1/2):";
-        cin>>a;
-        string c;
-        if(a==2){
+        cin>>choose;
+        string trigger;
+        if(choose==2){
             cout<<"특정 문자열을 입력하여 주십시요:";
-            cin>>c;
+            cin>>trigger;
         }
-        c.shrink_to_fit();
+        trigger.shrink_to_fit();
         for(auto& p: fs:: recursive_directory_iterator("./")){
-            string t1=p.path().filename();
-            if(c.empty()||t1.find(c)!=-1){
-                t1.erase(0,b);
-                rename(p,t1.c_str());
+            string temp1=p.path().filename();
+            if(trigger.empty()||temp1.find(trigger)!=-1){
+                if(temp1.find("a.out")!=-1||temp1.find("gibumfile.cpp")!=-1){
+                    continue;
+                }
+                temp1.erase(0,delposit);
+                rename(p,temp1.c_str());
             }
         }
     }
