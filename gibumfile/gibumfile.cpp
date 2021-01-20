83adc7d424db62c8448894bdbd12ab4aed2e0871
bbasung1 <bbasung1@daum.net>
2021-01-20 17:40:38 +0900

    develope earese function

--- gibumfile/gibumfile.cpp
+++ gibumfile/gibumfile.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 namespace fs=std::filesystem;
 int main(){
+    int a;
+    cout<<"다음중 선택세요(1.파일명 일괄 변경/2.특정 문자 지우기):";
+    cin>>a;
+    if( a==1){
     string target;
     string change;
     cout<<"바꾸려는 파일들에 공통적으로 들어 있는것은?";
@@ -26,6 +30,26 @@ int main(){
             final.shrink_to_fit();
             temp.shrink_to_fit();
             rename(temp.c_str(),final.c_str());
+        }
+    }
+    }
+    else if(a==2){
+        int b;
+        cout<<"앞에서부터 몇자리까지 지우실지를 입력해주세요:";
+        cin>>b;
+        cout<<"모든파일에 적용하시겠습니까?특정 문자열이 들어가 있을 경우 적용하시겠습니까?(1/2):";
+        cin>>a;
+        string c;
+        if(a==2){
+            cout<<"특정 문자열을 입력하여 주십시요:";
+            cin>>c;
+        }
+        c.shrink_to_fit();
+        for(auto& p: fs:: recursive_directory_iterator("./")){
+            string t1=p.path().filename();
+            if(c.capacity()==0){
+                
+            }
         }
     }
 }
