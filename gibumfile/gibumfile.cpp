1d0f430f1c6dab99e8ac3a1e2dcf58bd2dc6ef4f
bbasung1 <bbasung1@daum.net>
2021-01-20 16:45:17 +0900

    chane var and optimize

--- gibumfile/gibumfile.cpp
+++ gibumfile/gibumfile.cpp
@@ -5,22 +5,25 @@
 using namespace std;
 namespace fs=std::filesystem;
 int main(){
-    char a[5000];
-    string b;
+    char target[5000];
+    string change;
     cout<<"바꾸려는 파일의 이름들은?";
-    cin>>a;
+    cin>>target;
     cout<<"바꿀 이름은?";
-    cin>>b;
+    cin>>change;
+    int count=0;
     string path="./";
     for(auto& p: fs:: recursive_directory_iterator(path)){
-        string tp1=p.path().filename();
-        string t2=p.path().filename();
-        if(tp1.find(a)!=-1){          
-            int poin=tp1.find(".",1);
-            tp1.erase(0,poin);
-            string c=b;
-            c+=tp1;
-            rename(t2.c_str(),c.c_str());
+        string temp=p.path().filename();
+        if(temp.find(target)!=-1){          
+            int poin=temp.find(".",1);
+            temp.erase(0,poin);
+            string final=change+"0";
+            final+=to_string(count);
+            count+=1;
+            final+=temp;
+            temp=p.path().filename();
+            rename(temp.c_str(),final.c_str());
         }
     }
 }
